19320f2faf2e8ee04c72f283207b04cd4f734231
Arohan Dutt <arohandutt@gmail.com>
2020-12-12 23:25:41 -0500

    Fix shell script path

--- electron-pi.sh
+++ electron-pi.sh
@@ -5,4 +5,4 @@ sudo raspi-gpio set 8 a2
 # Build App
 USE_SYSTEM_FPM=true ./node_modules/.bin/electron-builder --linux --armv7l
 ./node_modules/.bin/electron-builder --prepackaged ./dist/linux-armv7l-unpacked/ --armv7l
-./temp-monitor-client-1.0.0-armv7l.AppImage
+./dist/temp-monitor-client-1.0.0-armv7l.AppImage
